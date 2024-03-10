85a52df1dc676b803d6688ec3b755909253c5e5a
Booker Ngoon <bookerngoon@gmail.com>
2024-03-10 17:41:14 -0400

    events.js/ functions to make api calls completed

--- src/services/events.js
+++ src/services/events.js
@@ -8,3 +8,40 @@ export const getEvents = async () => {
     console.error("Error getting all events: ", error);
   }
 };
+
+export const getEvent = async (eventName) => {
+  try {
+    const response = await api.get(`/events/${eventName}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error getting event", error);
+    throw error;
+  }
+};
+
+export const createEvent = async (eventData) => {
+  try {
+    const response = await api.post("/events", eventData);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating event", error);
+    throw error;
+  }
+};
+
+export const editEvent = async (`/events/${eventName}`, eventData) => {
+  try {
+      const response = await api.get(eventName, eventData)
+      return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const deleteEvent = async (eventName) => {
+  try {
+    const response = await api.delete(`/events/${eventName}`)
+  } catch (error) {
+    console.error(error)
+  }
+}
