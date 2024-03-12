1cba33ea07a7e90106caf401b02651ee0fffccb2
Booker Ngoon <bookerngoon@gmail.com>
2024-03-12 11:31:30 -0400

    css

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className='home-container'>
         <img src='https://www.state.gov/wp-content/uploads/2022/01/shutterstock_248799484-scaled.jpg' alt='new york busy intersection'/>
-    </div>
+    </div>  
     
   )
 }
